abc5527cded04c3760885b08e7af4c7eb3b63560
WitherOrNot <damemem@gmail.com>
2022-03-15 12:41:07 -0400

    Game now "launches" but not really

---
+++ natives/org_lwjgl_DefaultSysImplementation.js
@@ -0,0 +1,18 @@
+// This entire object is exported. Feel free to define private helper functions above it.
+registerNatives({
+  'org/lwjgl/DefaultSysImplementation': {
+
+    'getJNIVersion()I': function(thread, javaThis) {
+      return 19;
+    },
+
+    'getPointerSize()I': function(thread, javaThis) {
+      return 4;
+    },
+
+    'setDebug(Z)V': function(thread, javaThis, arg0) {
+      console.log('Ljava/lang/UnsatisfiedLinkError;', 'Native method not implemented.');
+    }
+
+  }
+});
